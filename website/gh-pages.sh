822b40ff9b7777a9b78608f11cd4c612e49cf367
Fabian Hauser <fabian@fh2.ch>
2017-06-28 16:50:13 +0200

    Fix gh-pages script to really work with sh only

--- website/gh-pages.sh
+++ website/gh-pages.sh
@@ -5,17 +5,16 @@ TARGET=$2
 BRANCH=$3
 
 # Add SSH key from env variable
-mkdir ~/.ssh/ &&
+mkdir -p ~/.ssh/ &&
     echo ${SSH_KEY} | base64 -d > ~/.ssh/id_ed25519 &&
     chmod -R 700 ~/.ssh/
 
-pushd $SOURCE
+cd $SOURCE
 git init
 git checkout -B "$BRANCH"
 git remote add origin "$TARGET"
 git add .
 git commit -m "Deployment" --author="Deployment <no-reply@redbackup.org>"
 git push -f
-popd
 
 echo "Website deployed."
