3fb9217b0a63be19711d41ec7459a3c85e92fe1d
Fabian Hauser <fabian@fh2.ch>
2017-06-28 17:17:09 +0200

    Travis: More fixes

--- website/gh-pages.sh
+++ website/gh-pages.sh
@@ -6,13 +6,13 @@ BRANCH=$3
 FQDN=$4
 
 # Add SSH key from env variable
-mkdir -p ~/.ssh/ &&
-    echo ${SSH_KEY} | base64 -d > ~/.ssh/id_ed25519 &&
-    chmod -R 700 ~/.ssh/
+mkdir -p ~/.ssh/
+echo ${SSH_KEY} | base64 -d > ~/.ssh/id_ed25519
+chmod -R 700 ~/.ssh/
 
 cd $SOURCE
 git init
-echo "$FQDN" > ${SOURCE}CNAME
+echo "$FQDN" > CNAME
 git add .
 git commit -m "Deployment" --author="Deployment <no-reply@redbackup.org>"
 git remote add origin "$TARGET"
